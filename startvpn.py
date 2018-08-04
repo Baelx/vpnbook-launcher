88b49d243edfa4b71cccf48c254862e993a3358c
Alex <awintschel@gmail.com>
2018-08-04 16:03:00 -0400

    just need to implement openvpn subprocess properly

--- startvpn.py
+++ startvpn.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 
 from PIL import Image
-from subprocess import call
-import pyperclip, pytesseract, requests, bs4, re
+import pyperclip, pytesseract, requests, bs4, re, subprocess
 
 print("Fetching credentials...")
 try:
@@ -13,7 +12,19 @@ try:
 
     match_img_src = re.compile(r'\"(.*?)\"')
     img_pass_src = match_img_src.search(str(passwd_img))
-    print(img_pass_src.group(1))
+    new_pic = requests.get('https://vpnbook.com/' + img_pass_src.group(1))
+    open('password.png', 'wb').write(new_pic.content)
 
 except IOError:
     print("WARNING: Couldn't reach vpnbook server. Will try to authenticate with existing password if it's found in the current directory.")
+
+try:
+    im = Image.open('password.png')
+    text = pytesseract.image_to_string(im)
+    print(text)
+except IOError:
+    print("ERROR: No password image found in current directory. Can't connect without password. Exiting.")
+
+subprocess.call(['sudo', '/usr/sbin/openvpn', '--config', 'vpnbook-us1-tcp80.ovpn'])
+
+
