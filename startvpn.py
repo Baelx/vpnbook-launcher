c3208c1511de9c6791befd79f017114895c8d887
Alex <awintschel@gmail.com>
2018-09-12 23:04:54 -0700

    Added more descriptive prompts, cleaned code

--- startvpn.py
+++ startvpn.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
 from PIL import Image
-import pyperclip, pytesseract, requests, bs4, re, subprocess
+import pyperclip, pytesseract, requests, bs4, re, subprocess, pexpect, sys, os
 
-print("Fetching credentials...")
+print("Fetching fresh credentials from vpnbook...)
 try:
     res = requests.get('https://vpnbook.com/freevpn')
     res.raise_for_status()
@@ -14,17 +14,20 @@ try:
     img_pass_src = match_img_src.search(str(passwd_img))
     new_pic = requests.get('https://vpnbook.com/' + img_pass_src.group(1))
     open('password.png', 'wb').write(new_pic.content)
-
+    print('SUCCESS')
 except IOError:
-    print("WARNING: Couldn't reach vpnbook server. Will try to authenticate with existing password if it's found in the current directory.")
+    print("WARNING: Couldn't reach vpnbook server.\nWill try to authenticate with existing password if it's found in the current directory.")
 
+print('Scanning image for password...)
 try:
     im = Image.open('password.png')
-    text = pytesseract.image_to_string(im)
-    print(text)
+    password_text = pytesseract.image_to_string(im)
+    print('SUCCESS')
 except IOError:
-    print("ERROR: No password image found in current directory. Can't connect without password. Exiting.")
+    print("ERROR: No password image found in current directory. Can't connect without password.\nExiting.")
 
 subprocess.call(['sudo', '/usr/sbin/openvpn', '--config', 'vpnbook-us1-tcp80.ovpn'])
 
-
+#child = pexpect.spawn('sudo /usr/sbin/openvpn --config vpnbook-us1-tcp80.opvn')
+#child.expect('Enter Auth Username:')
+#child.sendline('testing')
